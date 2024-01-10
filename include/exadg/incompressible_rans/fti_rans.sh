638e67a7aac1d9e5f21d6dbf55378d0303c53b37
bharan <bharansooraj@gmail.com>
2024-01-10 13:16:01 +0530

    chages the CMakeList

---
+++ include/exadg/incompressible_rans/fti_rans.sh
@@ -0,0 +1,3 @@
+find ./ -type f -exec sed -i 's/IncRANS/IncRANS/g' {} +
+find ./ -type f -exec sed -i 's/incompressible_rans/incompressible_rans/g' {} +
+find ./ -type f -exec sed -i 's/INCOMPRESSIBLE_RANS/INCOMPRESSIBLE_RANS/g' {} +
